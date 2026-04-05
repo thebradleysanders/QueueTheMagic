6a95d7b37923aa637fa380251284da3de15eb0b5
Brad Sanders <thebradleysanders@users.noreply.github.com>
2026-04-04 23:13:14 -0500

    fix crypto when not using https

--- web/src/stores/session.js
+++ web/src/stores/session.js
@@ -5,7 +5,11 @@ export const useSessionStore = defineStore('session', () => {
   const sessionToken = ref(localStorage.getItem('sessionToken') || generateToken())
 
   function generateToken() {
-    const token = crypto.randomUUID()
+    const token = typeof crypto.randomUUID === 'function'
+      ? crypto.randomUUID()
+      : Array.from(crypto.getRandomValues(new Uint8Array(16)))
+          .map((b, i) => ([4,6,8,10].includes(i) ? '-' : '') + b.toString(16).padStart(2, '0'))
+          .join('')
     localStorage.setItem('sessionToken', token)
     return token
   }
